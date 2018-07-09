5ff280fe185c2426d86b79b5becd8615fee274be
Reda Choukhmane <archoukhmane@gmail.com>
2018-07-09 22:24:59 +0200

    Final

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,16 +1,32 @@
-def translate(text)
+#def translate(text)
+#
+#	def consonant(text)
+#		while text.start_with?('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z') do
+#		text.slice!(0) << text[0] end
+#	end
+#consonant(text)
+#
+#	def vowel(text)
+#    	if text.start_with?('a','e','i','o','u','y')        
+#     		text << "ay" 
+#    	else text end
+#	end
+#vowel(text)
+#end
 
-	def consonant(text)
-		while text.start_with?('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z') do
-		text.slice!(0) << text[0] end
+def trans_word(phrase)
+	if phrase =~ /^[aeiou]/
+		phrase + "ay"
+	elsif phrase =~/^[^aeiou]*qu/
+		phrase.delete(phrase.slice(/^[^aeiou]*qu/)).to_s + phrase.slice(/^[^aeiou]*qu/).to_s + "ay"
+	else
+		phrase.slice(/[aeiou].*$/) + phrase.slice(/^[^aeiou]{1,}/) + "ay"
 	end
-consonant(text)
+end
 
-
-	def vowel(text)
-    	if text.start_with?('a','e','i','o','u','y')        
-     		text << "ay" 
-    	else text end
+def translate(phrase)
+	if phrase.split.length > 1
+		phrase.split.map{|x| trans_word(x)}.join(" ")
+	else trans_word(phrase)
 	end
-vowel(text)
 end
