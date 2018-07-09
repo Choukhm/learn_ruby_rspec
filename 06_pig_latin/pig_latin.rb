feb2edf9ad911d6682c06befec8870cfcd95abc6
Reda Choukhmane <archoukhmane@gmail.com>
2018-07-09 22:26:42 +0200

    Version Final

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -14,19 +14,19 @@
 #vowel(text)
 #end
 
-def trans_word(phrase)
-	if phrase =~ /^[aeiou]/
-		phrase + "ay"
-	elsif phrase =~/^[^aeiou]*qu/
-		phrase.delete(phrase.slice(/^[^aeiou]*qu/)).to_s + phrase.slice(/^[^aeiou]*qu/).to_s + "ay"
+def trans_word(text)
+	if text =~ /^[aeiou]/
+		text + "ay"
+	elsif text =~/^[^aeiou]*qu/
+		text.delete(text.slice(/^[^aeiou]*qu/)).to_s + text.slice(/^[^aeiou]*qu/).to_s + "ay"
 	else
-		phrase.slice(/[aeiou].*$/) + phrase.slice(/^[^aeiou]{1,}/) + "ay"
+		text.slice(/[aeiou].*$/) + text.slice(/^[^aeiou]{1,}/) + "ay"
 	end
 end
 
-def translate(phrase)
-	if phrase.split.length > 1
-		phrase.split.map{|x| trans_word(x)}.join(" ")
-	else trans_word(phrase)
+def translate(text)
+	if text.split.length > 1
+		text.split.map{|x| trans_word(x)}.join(" ")
+	else trans_word(text)
 	end
 end
